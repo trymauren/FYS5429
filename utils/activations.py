6f5971a3bb1cb08e1035a211370e58a9ab63dee1
Trym Auren <trym@eduroam-193-157-227-44.wlan.uio.no>
2024-01-29 12:13:36 +0100

    Setup

--- utils/activations.py
+++ utils/activations.py
@@ -71,29 +71,3 @@ class Sigmoid(Activation):
         return z*(1 - z)
 
 
-
-def test():
-    
-    relu = Relu()
-    sigmoid = Sigmoid()
-    tanh = Tanh()
-
-    z = np.ones((5,2))
-    assert(relu(z).shape==z.shape)
-    assert(tanh(z).shape==z.shape)
-    assert(sigmoid(z).shape==z.shape)
-
-    z = np.array([-100,-2,-1,0,1,2,100])
-    assert(relu(z).all() == np.array([0,0,0,0,1,2,100]).all())
-    assert(tanh(z).all() == np.array([-1.0,-0.9640275800758169,-0.7615941559557649,
-                                0.0,0.7615941559557649,0.9640275800758169,1.0]).all())
-
-    np.testing.assert_allclose(sigmoid(z) == np.array([0.0,0.11920292202211755,
-                                                             0.26894142136999512,
-                                                             0.5,
-                                                             0.73105857863000487,
-                                                             0.880797077977882444,
-                                                             0.999999999999999]), 1e-10,1e10)
-
-test()
-
---
+++ utils/loss_functions.py
@@ -0,0 +1,43 @@
+import numpy as np
+import git
+path_to_root = git.Repo('.', search_parent_directories=True).working_dir
+
+LOG_CONST = 1e-15 # why this number? Many use it
+
+class LossFunction():
+
+    def __call__(self, y, y_pred):
+        return self.eval(y, y_pred)
+
+class Mean_Square_loss(LossFunction):
+    
+    def __init__(self):
+        super().__init__()
+
+    def eval(self, y, y_pred):
+        # this may be the sklearn implementation?
+        return np.square(np.subtract(y, y_pred)).mean()
+
+class Classification_logloss(LossFunction):
+    
+    def __init__(self, n_classes):
+        self.n_classes = n_classes
+        super().__init__()
+
+    def eval(self, y, y_pred):
+        y_pred = y_pred + LOG_CONST # to avoid log(0) calculations
+        return -np.sum(y*np.log(y_pred))
+
+
+
+# remove this later
+y_true = np.array([2,4,5,1,2,4])
+y_pred = np.array([1,3,5,2,2,3])
+unique = np.unique(y_true)
+mcl = Classification_logloss(unique)
+loss = mcl(y_true,y_pred)
+print('Classification logloss: {0}'.format(loss))
+
+msl = Mean_Square_loss()
+mse = msl(y_true,y_pred)
+print('Regression logloss: {0}'.format(mse))
