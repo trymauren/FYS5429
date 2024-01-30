fe233e9f2d2763a01b6404c35d49cb10f20f1de0
Mons <monsbjerkan@gmail.com>
2024-01-30 18:15:22 +0100

    Further work on forward pass

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -5,12 +5,23 @@ class RecurrentNN:
     def __init__(self) -> None:
         pass
 
-    def forward(self,X:np.array, w_x:np.array, w_rec:np.array) -> np.array:
+    def forward(self,X:np.array, W_x:np.array, W_rec:np.array) -> np.array:
+        
+        time_steps = X.shape[0]
+        h_layer_size = W_rec.shape[0]
 
-        h_states = np.zeros((len(X)),self.n_hidden_nodes)        
+        h_states = np.zeros(time_steps,h_layer_size)        
+        
+        z = X[t,:]@W_x
+        h_t = self.act(z)
+        h_states[0,:] = np.flatten(h_t)
 
-        for i in range(len(X)):
-            h_states[i,:] = X[i,:]@w_x + h_states[i-1,:]@w_rec
+        for t in range(1,time_steps):
+            z = X[t,:]@W_x + h_states[t-1,:]@W_rec
+            h_t = self.act(z)
+        
+        h_states[t,:] = np.flatten(h_t)
+
         
 
     def _backward(self, y_estimate : np.ndarray) -> None:
@@ -24,7 +35,9 @@ class RecurrentNN:
             optimization_algorithm : str = None
         ) -> None:
         
-        w_hh= np.zeros(self.n_hidden_nodes, self.n_hidden_nodes) #State values for each node in each state
+        W_xh = np.random.randn(hidden_size, num_chars)*0.01     # weight input -> hidden. 
+        W_hh = np.random.randn(hidden_size, hidden_size)*0.01   # weight hidden -> hidden
+        W_hy = np.random.randn(num_chars, hidden_size)*0.01     # weight hidden -> output
 
     def predict(self, X : np.ndarray) -> np.ndarray:
         pass
