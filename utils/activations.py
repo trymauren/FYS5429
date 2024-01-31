450972acbfe038b763718a297c97d160b588113a
Trym Auren <trym.auren@gmail.com>
2024-01-31 17:44:00 +0100

    Small change to class-variables

--- utils/activations.py
+++ utils/activations.py
@@ -32,27 +32,36 @@ class Relu(Activation):
         """Returns a np.ndarray with same dimensions as z"""
         # ret = [n * (n > 0) for n in z]
         # return np.array(ret, dtype=float)
-        return np.maximum(0,z)
+        self.z = np.maximum(0,z)
+        return self.z
 
     def grad(self, z):
         """Decided on using f'(0)=1. Could do f'(0)=0 instead"""
         return 1 if z >= 0 else 0
 
 class Tanh(Activation):
+    
+    self.z = None
 
     def __init__(self):
         super().__init__()
 
     def eval(self, z):
         """Returns a np.ndarray with same dimensions as z"""
-        return np.tanh(z)
+        self.z = np.tanh(z)
+        return self.z 
 
     def grad(self, z):
         """
         Assume z is the output from tanh(z)! or else the
         derivative must be calculated differently
         """
-        return 1 - z^2
+        if self.z == None:
+            print('Error, something should be thrown here')
+            return None
+            
+        self.z = None
+        return 1 - self.z^2
 
 class Sigmoid(Activation): 
 
@@ -63,11 +72,11 @@ class Sigmoid(Activation):
         """Returns a np.ndarray with same dimensions as z"""
         return expit(z)
 
-    def grad(self, z):
+    def grad(self, a):
         """
         Assume z is the output from sigmoid(z)! or else the
         derivative must be calculated differently
         """
-        return z*(1 - z)
+        return a*(1 - a)
 
 
