deafcba30fd80dbf217235e17993d77bf212e6bb
Trym Auren <trym.auren@gmail.com>
2024-02-01 14:02:10 +0100

    Fix some faults in the setup

--- utils/activations.py
+++ utils/activations.py
@@ -8,6 +8,7 @@ Creds to "https://github.com/ddbourgin/numpy-ml/blob/master/numpy_ml/
 neural_nets/activations/activations.py#L73-L137" for the setup
 """
 
+
 class Activation():
     def __init__(self):
         super().__init__()
@@ -23,6 +24,7 @@ class Activation():
     def grad(self, z):
         raise NotImplementedError
 
+
 class Relu(Activation):
 
     def __init__(self):
@@ -32,16 +34,22 @@ class Relu(Activation):
         """Returns a np.ndarray with same dimensions as z"""
         # ret = [n * (n > 0) for n in z]
         # return np.array(ret, dtype=float)
-        self.z = np.maximum(0,z)
+        self.z = np.maximum(0, z)
         return self.z
 
     def grad(self, z):
         """Decided on using f'(0)=1. Could do f'(0)=0 instead"""
-        return 1 if z >= 0 else 0
+
+        if self.z is None:
+            print('Error, something should be thrown here')
+            return None
+
+        ret = 1 if z >= 0 else 0
+        self.z = None
+        return ret
+
 
 class Tanh(Activation):
-    
-    self.z = None
 
     def __init__(self):
         super().__init__()
@@ -49,21 +57,23 @@ class Tanh(Activation):
     def eval(self, z):
         """Returns a np.ndarray with same dimensions as z"""
         self.z = np.tanh(z)
-        return self.z 
+        return self.z
 
     def grad(self, z):
         """
         Assume z is the output from tanh(z)! or else the
         derivative must be calculated differently
         """
-        if self.z == None:
+        if self.z is None:
             print('Error, something should be thrown here')
             return None
-            
-        self.z = None
-        return 1 - self.z^2
 
-class Sigmoid(Activation): 
+        ret = 1 - self.z**2
+        self.z = None
+        return ret
+
+
+class Sigmoid(Activation):
 
     def __init__(self):
         super().__init__()
--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,15 +1,20 @@
-import numpy as np
+import sys
 import git
 path_to_root = git.Repo('.', search_parent_directories=True).working_dir
+sys.path.append(path_to_root)
+
+import numpy as np
+from collections.abc import Callable 
+
 
 LOG_CONST = 1e-15 # why this number? Many use it
 
-class LossFunction():
+class LossFunction(Callable):
 
     def __call__(self, y, y_pred):
         return self.eval(y, y_pred)
 
-class Mean_Square_loss(LossFunction):
+class Mean_Square_Loss(LossFunction):
     
     def __init__(self):
         super().__init__()
@@ -18,7 +23,7 @@ class Mean_Square_loss(LossFunction):
         # this may be the sklearn implementation?
         return np.square(np.subtract(y, y_pred)).mean()
 
-class Classification_logloss(LossFunction):
+class Classification_Logloss(LossFunction):
     
     def __init__(self, n_classes):
         self.n_classes = n_classes
@@ -30,14 +35,14 @@ class Classification_logloss(LossFunction):
 
 
 
-# remove this later
-y_true = np.array([2,4,5,1,2,4])
-y_pred = np.array([1,3,5,2,2,3])
-unique = np.unique(y_true)
-mcl = Classification_logloss(unique)
-loss = mcl(y_true,y_pred)
-print('Classification logloss: {0}'.format(loss))
+# # remove this later
+# y_true = np.array([2,4,5,1,2,4])
+# y_pred = np.array([1,3,5,2,2,3])
+# unique = np.unique(y_true)
+# mcl = Classification_logloss(unique)
+# loss = mcl(y_true,y_pred)
+# print('Classification logloss: {0}'.format(loss))
 
-msl = Mean_Square_loss()
-mse = msl(y_true,y_pred)
-print('Regression logloss: {0}'.format(mse))
+# msl = Mean_Square_loss()
+# mse = msl(y_true,y_pred)
+# print('Regression logloss: {0}'.format(mse))
