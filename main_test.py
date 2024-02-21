cbd6d605596f0b6198013131059e84baf833eea9
Trym Auren <trym.auren@gmail.com>
2024-02-21 19:40:46 +0100

    Revert "Add correct dim of test-array"
    This reverts commit a6c0a37677ba4f61be666f28c442e17a7b6b5257.

--- main_test.py
+++ main_test.py
@@ -2,185 +2,19 @@ import sys
 import git
 import numpy as np
 from rnn.rnn import ReccurentNN
-from utils.activations import Relu, Softmax, Tanh
+from utils.activations import Relu
+
 path_to_root = git.Repo('.', search_parent_directories=True).working_dir
 sys.path.append(path_to_root)
 
-rnn = ReccurentNN(Relu(), Softmax())
-# y_true = np.array([[[1, 2, 3, 4, 5]], [[2, 3, 4, 5, 6]], [3, 4, 5, 6, 7]])
-# x = np.array([[[0, 1, 2, 3, 4]], [[1, 2, 3, 4, 5]], [[2, 3, 4, 5, 6]]], dtype=float)
-# y_true = np.array([[[1, 2], [1, 3]], [[1, 2], [1, 3]]])
-# x = np.array([[[1, 2], [1, 3]], [[1, 2], [1, 3]]], dtype=float)
-# y_true = np.array([[[1], [1]], [[1], [1]]])
-# x = np.array([[[1], [1]], [[1], [1]]], dtype=float)
+rnn = ReccurentNN(Relu())
+y_true = np.array([[1, 3, 2, 4, 5], [3, 2, 4, 1, 5]])
+x = np.array([[[0, 1, 3, 2, 4]], [[0, 3, 2, 4, 1]]], dtype=float)
+# y_true = np.ones((2, 26))
+# x = np.ones((2, 3, 1))
+rnn.fit(x, y_true, 100, 0.1)
 
-# y_true = np.array([[[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[1, 1, 2, 2]],
-#                    [[2, 2, 3, 3]]], dtype=int)
+# print(np.array([1,1]).reshape(-1,1).shape)
 
-# x = np.array([[[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[0, 1, 2, 3]],
-#               [[1, 2, 3, 4]]], dtype=float)
-# rnn.fit(x, y_true, 100, 0.1)
-
-
-def file_to_single_string(file_path):
-    with open(file_path, 'r') as file:
-        # Read lines, remove newlines and any empty or whitespace-only lines
-        lines = [line.strip() for line in file if line.strip()]
-        # Concatenate all lines into a single string
-        single_string = ' '.join(lines)
-    return single_string
-
-
-# Example usage
-file_path = path_to_root + '/data/cat_text'
-text_string = file_to_single_string(file_path)
-
-data = text_string
-chars = list(set(data))
-
-char_to_ix = {ch: i for i, ch in enumerate(chars)}
-ix_to_char = {i: ch for i, ch in enumerate(chars)}
-print(ix_to_char)
-n = 0
-p = 0
-n_chars = len(chars)
-
-y_true = []
-X_train = []
-while n < 100:
-    if p + n_chars + 1 >= len(data) or n == 0:
-        p = 0
-    inputs = [char_to_ix[ch] for ch in data[p:p+n_chars]]
-    targets = [char_to_ix[ch] for ch in data[p+1:p+n_chars+1]]
-    y_true.append(np.array(targets))
-    X_train.append(np.array(inputs))
-    p += n_chars  # move data pointer
-    n += 1  # iteration counter
-
-X_train = np.array(X_train)
-
-y_true = np.array(y_true)
-
-rnn.fit(X_train, y_true, 100, 0.1)
-
-ret = rnn.predict(X_train, 1, 100)
-txt = ''.join(ix_to_char[ix] for ix in ret)
-print('----\n %s \n----' % (txt, ))
+le = np.array([[2, 2], [1, 1]])
+print(le[0])
