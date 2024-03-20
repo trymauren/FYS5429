966b4c75c415d93997944e165812f3db540db56d
Mons <monsbjerkan@gmail.com>
2024-03-20 19:42:30 +0100

    Several changes, added script for test run, yaml config construction/parsing, small embedding data test file. Got predict() to work in rnn.py, implemented (naive?) gradient clipping in optimisers.py

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -1,18 +1,24 @@
+from importlib.resources import open_text
+from pathlib import Path
 import sys
+import os
+from typing import Dict
 import git
 import numpy as np
 from collections.abc import Callable
+import yaml
 from utils.activations import Relu, Tanh
 from utils.loss_functions import Mean_Square_Loss as mse
 from utils.optimisers import Adam
 from utils import read_load_model
+from utils.word_embedding import word_embedding
 import matplotlib.pyplot as plt
 
 path_to_root = git.Repo('.', search_parent_directories=True).working_dir
 sys.path.append(path_to_root)
+np.random.seed(24)
 
-
-class ReccurentNN:
+class RNN:
 
     def __init__(
             self,
@@ -21,25 +27,26 @@ class ReccurentNN:
             loss_function: Callable = None,
             optimiser: Callable = None,
             regression: bool = False,
-            classification: Callable = False,
-            name: str = 'rnn') -> None:
+            classification: bool = False,
+            name: str = 'rnn',
+            config: Dict | Path | str = 'default',
+            ) -> None:
 
         # Setting activation functions, loss function and optimiser
         if not hidden_activation:
             hidden_activation = Relu()
-        self._hidden_activation = hidden_activation
-
+        self._hidden_activation = eval(hidden_activation)
         if not output_activation:
             output_activation = Tanh()
-        self._output_activation = output_activation
+        self._output_activation = eval(output_activation)
 
         if not loss_function:
             loss_function = mse()
-        self._loss_function = loss_function
+        self._loss_function = eval(loss_function)
 
         if not optimiser:
             optimiser = Adam()
-        self._optimiser = optimiser
+        self._optimiser = eval(optimiser)
 
         self.regression = regression
         self.classification = classification
@@ -56,6 +63,27 @@ class ReccurentNN:
 
         self.name = name
 
+    def windowed_data(self, X : np.ndarray, num_hidden_states : int) ->\
+    np.ndarray:
+        split_X = []
+        split_y = []
+        for t in range(len(X)-num_hidden_states-1):
+            split_X.append(X[t:(t + num_hidden_states)])
+            split_y.append(X[t+1: t+num_hidden_states+1])
+
+        return split_X, split_y
+    
+    def format_data(self, X, is_text : bool):
+        formatted_X = None
+        if is_text:
+            formatted_X = word_embedding.read_txt(X)
+        else:
+            formatted_X = []
+            for data in zip(X):
+                formatted_X.append(float(data))
+            formatted_X = np.array(formatted_X)
+        return formatted_X
+    
     def _forward(
             self,
             X_partition,
@@ -89,14 +117,19 @@ class ReccurentNN:
             h_t = self._hidden_activation(z)
             self.hs[t] = h_t
             self.ys[t] = self._output_activation(self.w_hy @ self.hs[t])
-            self._loss_function(self.ys[t], y_partition[t])
+            #Added check if y is None to avoid calculating loss when predicting
+            #and also have a consistent value to pass a y parameter when 
+            #calling forward() in predict
+            if y_partition is not None:
+                self._loss_function(self.ys[t], y_partition[t])
         return self.ys
 
+    
     def _backward(
             self,
             y_true,
             y_pred,
-            num_hidden_layers) -> None:
+            num_hidden_nodes) -> None:
 
         deltas_w_xh = np.zeros_like(self.w_xh, dtype=float)
         deltas_w_hh = np.zeros_like(self.w_hh, dtype=float)
@@ -105,7 +138,7 @@ class ReccurentNN:
         deltas_b_hh = np.zeros_like(self.b_hh, dtype=float)
         deltas_b_hy = np.zeros_like(self.b_hy, dtype=float)
 
-        prev_grad_h_Cost = np.zeros_like(num_hidden_layers)
+        prev_grad_h_Cost = np.zeros_like(num_hidden_nodes)
 
         loss = self._loss_function(y_true, y_pred)
         loss_grad = self._loss_function.grad(loss=loss)
@@ -184,11 +217,12 @@ class ReccurentNN:
         #     )
 
     def fit(self,
-            X: np.ndarray,
-            y: np.ndarray,
-            epochs: int,
+            X: np.ndarray = None,
+            y: np.ndarray = None,
+            epochs: int = None,
             num_hidden_states: int = 5,
-            num_hidden_layers: int = 5,
+            num_hidden_nodes: int = 5,
+            is_text : bool = False,
             ) -> None:
         """
         Method for training the RNN, iteratively runs _forward(), and
@@ -217,13 +251,16 @@ _
         num_hidden_states : int
             - Number of times to unroll the rnn architecture
 
-        num_hidden_layers : int
+        num_hidden_nodes : int
             - Number of fully connected layers to add
 
         Returns:
         -------------------------------
         None
         """
+        X = np.array(X)
+        y = np.array(y)
+
         time_steps, num_features = X.shape
         time_steps_y, output_size = y.shape
 
@@ -233,34 +270,38 @@ _
         self.init_states(
             output_size,
             num_hidden_states,
-            num_hidden_layers=num_hidden_layers
+            num_hidden_nodes=num_hidden_nodes
         )
         self.init_weights(
             num_features,
             output_size,
-            num_hidden_layers=num_hidden_layers
+            num_hidden_nodes=num_hidden_nodes
         )
 
         # Split data into "batches"
+        #TODO exchange with windowed_data() ??????? Update:
+        #It fucked up, has to look through again
         partitions = np.floor(time_steps/num_hidden_states)
         X_split = np.split(X, partitions, axis=0)
         y_split = np.split(y, partitions, axis=0)
 
+        #X_split,y_split = self.windowed_data(X,num_hidden_states)
+
+
         # Run training
         for e in range(epochs):
-
+            print(self.w_xh)
             for X_partition, y_partition in zip(X_split, y_split):
-
                 y_pred = self._forward(
                     X_partition,
                     y_partition,
                     num_hidden_states
                 )
-
+                print(y_pred)
                 self._backward(
                     y_partition,
                     y_pred,
-                    num_hidden_layers
+                    num_hidden_nodes
                 )
 
         read_load_model.save_model(  # pickle dump the trained estimator
@@ -291,13 +332,17 @@ _
         np.array
         - Predicted next value for the given input sequence
         """
-        return self._forward(X)[-1]
+
+        #Have to pass Y (as None to avoid calculating loss when only 
+        #doing a prediction (less computational power required *sunglass
+        #emoji*)) and number of hidden nodes to make forward() run
+        return self._forward(X,None, self.num_hidden_nodes)[-1]
 
     def init_weights(
             self,
             num_features,
             output_size,
-            num_hidden_layers=1,
+            num_hidden_nodes=1,
             scale=0.1) -> None:
         """
         Initialises weights and biases and assign them to instance variables.
@@ -310,7 +355,7 @@ _
         output_size : int
             - Number of entries in each label
 
-        num_hidden_layers : int
+        num_hidden_nodes : int
             - Number of fully connected layers to add
 
         Returns:
@@ -322,22 +367,22 @@ _
         # w_hh = hidden_layers x hidden_layers = 1 x 1, h = 1 x 1 (per state)
         # w_hy = 1 x hidden_layers = 1 x 1, y = 1 x 1 (per state)
         self.w_xh = np.random.randn(
-            num_hidden_layers, num_features) * scale
+            num_hidden_nodes, num_features) * scale
         self.w_hh = np.random.randn(
-            num_hidden_layers, num_hidden_layers) * scale
+            num_hidden_nodes, num_hidden_nodes) * scale
         self.w_hy = np.random.randn(
-            output_size, num_hidden_layers) * scale
+            output_size, num_hidden_nodes) * scale
 
         self.b_hh = np.random.randn(
-            num_hidden_layers, num_hidden_layers) * scale
+            num_hidden_nodes, num_hidden_nodes) * scale
         self.b_hy = np.random.randn(
-            output_size, num_hidden_layers) * scale
+            output_size, num_hidden_nodes) * scale
 
     def init_states(
             self,
             output_size,
             num_hidden_states,
-            num_hidden_layers=1) -> None:
+            num_hidden_nodes=1) -> None:
         """
         Initialises states and assign them to instance variables.
 
@@ -348,13 +393,15 @@ _
               This essentially means how many forward and backward steps
               are performed per data-partition (per batch).
 
-        num_hidden_layers : int
+        num_hidden_nodes : int
             - Number of fully connected layers to add
 
         Returns:
         -------------------------------
         None
         """
-        self.hs = np.zeros((num_hidden_states, num_hidden_layers))
-        self.xs = np.zeros((num_hidden_states, num_hidden_layers))
+        self.num_hidden_nodes = num_hidden_nodes
+        self.hs = np.zeros((num_hidden_states, num_hidden_nodes))
+        self.xs = np.zeros((num_hidden_states, num_hidden_nodes))
         self.ys = np.zeros((num_hidden_states, output_size))
+        
