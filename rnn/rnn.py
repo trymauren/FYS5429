95875c863d3b0362480176e7208c97968a508518
Trym Auren <trym.auren@gmail.com>
2024-03-23 00:42:08 +0100

    Some bigger changes to processing in fit and predict

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -9,14 +9,16 @@ from collections.abc import Callable
 import yaml
 from utils.activations import Relu, Tanh
 from utils.loss_functions import Mean_Square_Loss as mse
-from utils.optimisers import Adam
+from utils.optimisers import SGD, SGD_momentum, AdaGrad
 from utils import read_load_model
 from utils.word_embedding import word_embedding
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 path_to_root = git.Repo('.', search_parent_directories=True).working_dir
 sys.path.append(path_to_root)
 np.random.seed(24)
+
 
 class RNN:
 
@@ -47,7 +49,7 @@ class RNN:
         self._loss_function = eval(loss_function)
 
         if not optimiser:
-            optimiser = Adam()
+            optimiser = AdaGrad()
         self._optimiser = eval(optimiser)
 
         self.regression = regression
@@ -63,14 +65,20 @@ class RNN:
 
         self.xs, self.hs, self.ys = None, None, None
 
+        self.built = False
+
         self.name = name
 
         self.stats = {
             'other stuff': [],
         }
 
-    def windowed_data(self, X : np.ndarray, num_hidden_states : int) ->\
-    np.ndarray:
+    def windowed_data(
+            self,
+            X: np.ndarray,
+            num_hidden_states: int
+            ) -> np.ndarray:
+
         split_X = []
         split_y = []
         for t in range(len(X)-num_hidden_states-1):
@@ -78,8 +86,13 @@ class RNN:
             split_y.append(X[t+1: t+num_hidden_states+1])
 
         return split_X, split_y
-    
-    def format_data(self, X, is_text : bool):
+
+    def format_data(
+            self,
+            X,
+            is_text: bool
+            ) -> None:
+
         formatted_X = None
         if is_text:
             formatted_X = word_embedding.read_txt(X)
@@ -89,11 +102,10 @@ class RNN:
                 formatted_X.append(float(data))
             formatted_X = np.array(formatted_X)
         return formatted_X
-    
+
     def _forward(
             self,
             X_partition,
-            y_partition,
             num_hidden_states) -> None:
         """
         Forward-pass method to be used in fit-method for training the
@@ -104,9 +116,6 @@ class RNN:
         X_partition:
             - A partition of samples
 
-        y_partition:
-            - A partition of labels
-
         num_hidden_states : int
             - Number of times to unroll the rnn architecture
 
@@ -114,7 +123,6 @@ class RNN:
         -------------------------------
         None
         """
-        None
         for t in range(num_hidden_states):
             x_weighted = self.w_xh @ X_partition[t]
             h_weighted = self.w_hh @ self.hs[t-1]
@@ -125,12 +133,7 @@ class RNN:
             self.ys[t] = self._output_activation(self.w_hy @ self.hs[t])
         return self.ys
 
-    
-    def _backward(
-            self,
-            y_true,
-            y_pred,
-            num_hidden_nodes) -> None:
+    def _backward(self, num_backsteps=10) -> None:
 
         deltas_w_xh = np.zeros_like(self.w_xh, dtype=float)
         deltas_w_hh = np.zeros_like(self.w_hh, dtype=float)
@@ -139,12 +142,11 @@ class RNN:
         deltas_b_hh = np.zeros_like(self.b_hh, dtype=float)
         deltas_b_hy = np.zeros_like(self.b_hy, dtype=float)
 
-        prev_grad_h_Cost = np.zeros_like(num_hidden_nodes)
+        prev_grad_h_Cost = np.zeros_like(self.num_hidden_nodes)
 
-        loss = self._loss_function(y_true, y_pred)
-        loss_grad = self._loss_function.grad(loss=loss)
-
-        for t in range(len(self.hs)-1, -1, -1):
+        loss_grad = self._loss_function.grad()
+        num_backsteps = min(len(self.hs)-1, num_backsteps)
+        for t in range(num_backsteps, -1, -1):
 
             """ BELOW IS CALCULATION OF GRADIENTS W/RESPECT TO HIDDEN_STATES
             - SEE (1-~20) IN TEX-DOCUMENT """
@@ -202,29 +204,32 @@ class RNN:
             To emphasize: the part before the "+" in 10.21 in DLB"""
             prev_grad_h_Cost = d_act @ self.w_hh.T @ grad_h_Cost
 
-        # Weight updates:
-        self.w_hy += 0.01 * deltas_w_hy
-        self.w_hh += 0.01 * deltas_w_hh
-        self.w_xh += 0.01 * deltas_w_xh
-        # Bias updates
-        self.w_hy += 0.01 * deltas_b_hy
-        self.w_hh += 0.01 * deltas_b_hh
-        # self._optimiser(
-        #         deltas_w_hy=deltas_w_hy,
-        #         deltas_w_hh=deltas_w_hh,
-        #         deltas_w_xh=deltas_w_xh,
-        #         deltas_b_hy=deltas_b_hy,
-        #         deltas_b_hh=deltas_b_hh,
-        #     )
+        # # Weight updates:
+        # self.w_hy += 0.01 * deltas_w_hy
+        # self.w_hh += 0.01 * deltas_w_hh
+        # self.w_xh += 0.01 * deltas_w_xh
+        # # Bias updates
+        # self.w_hy += 0.01 * deltas_b_hy
+        # self.w_hh += 0.01 * deltas_b_hh
+        params = [self.w_hy, self.w_hh, self.w_xh,
+                  self.b_hy, self.b_hh]
+        deltas = [deltas_w_hy, deltas_w_hh, deltas_w_xh,
+                  deltas_b_hy, deltas_b_hh]
+        steps = self._optimiser(deltas)
+
+        for param, step in zip(params, steps):
+            param -= step
 
     def fit(self,
             X: np.ndarray = None,
             y: np.ndarray = None,
             epochs: int = None,
+            batches: int = 5,
+            is_text: bool = False,
+            learning_rate: float = 0.01,
             num_hidden_states: int = 5,
             num_hidden_nodes: int = 5,
-            is_text : bool = False,
-            learning_rate: float = 0.01
+            num_backsteps: float = 0.01,
             ) -> None:
         """
         Method for training the RNN, iteratively runs _forward(), and
@@ -260,52 +265,44 @@ _
         -------------------------------
         None
         """
+
         X = np.array(X)
         y = np.array(y)
 
-        time_steps, num_features = X.shape
-        time_steps_y, output_size = y.shape
+        examples, time_steps, num_features = X.shape
+        examples, time_steps_y, output_size = y.shape
 
-        assert (time_steps == time_steps_y)
-        assert (num_hidden_states <= time_steps)
+        self.output_size = output_size
+        self.num_features = num_features
+        self.num_hidden_nodes = num_hidden_nodes
 
-        self.init_states(
-            output_size,
-            num_hidden_states,
-            num_hidden_nodes=num_hidden_nodes
-        )
-        self.init_weights(
-            num_features,
-            output_size,
-            num_hidden_nodes=num_hidden_nodes
-        )
+        self.init_states(time_steps)
 
-        # Split data into "batches"
-        #TODO exchange with windowed_data() ??????? Update:
-        #It fucked up, has to look through again
-        partitions = np.floor(time_steps/num_hidden_states)
-        X_split = np.split(X, partitions, axis=0)
-        y_split = np.split(y, partitions, axis=0)
-        # X_split,y_split = self.windowed_data(X,num_hidden_states)
+        self.init_weights()
+
+        X_split = np.split(X, batches)
+        y_split = np.split(y, batches)
+
         self.stats['loss'] = [0]*epochs
-        # Run training
-        for e in range(epochs):
 
-            for idx, (X_partition, y_partition) in enumerate(zip(X_split, y_split)):
+        for e in tqdm(range(epochs)):
 
-                y_pred = self._forward(
-                    X_partition,
-                    y_partition,
-                    num_hidden_states
-                )
-                print(y_pred)
+            for x_batch, y_batch in zip(X_split, y_split):
+
+                y_pred_batch = np.zeros_like(y_batch)
+
+                for idx, (x, y) in enumerate(zip(x_batch, y_batch)):
+                    y_pred = self._forward(
+                        x,
+                        num_hidden_states
+                    )
+                    y_pred_batch[idx] = y_pred
+
+                    self.loss(self.ys, y, e)
+
                 self._backward(
-                    y_partition,
-                    y_pred,
-                    num_hidden_nodes
+                    num_backsteps=10
                 )
-
-                self.loss(self.ys, y_partition, idx)
 
         read_load_model.save_model(  # pickle dump the trained estimator
             self,
@@ -335,34 +332,32 @@ _
         np.array
         - Predicted next value for the given input sequence
         """
+        X = np.array(X)
+        examples, time_steps, features = X.shape
+        assert (self.num_features == features)  # cannot change number of features at predict time
+        self.init_states(time_steps)
+        ret = np.zeros((examples, self.output_size))
+        for example in range(examples):
+            prev_h = np.copy(self.hs[-1])
+            y_pred = self._forward(
+                X[example],
+                time_steps
+            )
+            self.hs[-1] = prev_h
+            ret[example] = y_pred[-1]
 
-        #Have to pass Y (as None to avoid calculating loss when only 
-        #doing a prediction (less computational power required *sunglass
-        #emoji*)) and number of hidden nodes to make forward() run
-        # Trym: loss calculation is moved to backwards,
-        # so no need to pass argument
-        return self._forward(X,None, self.num_hidden_nodes)[-1]
+        return ret
 
     def init_weights(
             self,
-            num_features,
-            output_size,
-            num_hidden_nodes=1,
             scale=0.1) -> None:
         """
         Initialises weights and biases and assign them to instance variables.
 
         Parameters:
         -------------------------------
-        num_features : int
-            - Number of entries in each time-sample (time-step)
-
-        output_size : int
-            - Number of entries in each label
-
-        num_hidden_nodes : int
-            - Number of fully connected layers to add
-
+        scale : float
+            - scaling of init weights
         Returns:
         -------------------------------
         None
@@ -372,22 +367,21 @@ _
         # w_hh = hidden_layers x hidden_layers = 1 x 1, h = 1 x 1 (per state)
         # w_hy = 1 x hidden_layers = 1 x 1, y = 1 x 1 (per state)
         self.w_xh = np.random.randn(
-            num_hidden_nodes, num_features) * scale
+            self.num_hidden_nodes, self.num_features) * scale
         self.w_hh = np.random.randn(
-            num_hidden_nodes, num_hidden_nodes) * scale
+            self.num_hidden_nodes, self.num_hidden_nodes) * scale
         self.w_hy = np.random.randn(
-            output_size, num_hidden_nodes) * scale
+            self.output_size, self.num_hidden_nodes) * scale
 
         self.b_hh = np.random.randn(
-            num_hidden_nodes, num_hidden_nodes) * scale
+            self.num_hidden_nodes, self.num_hidden_nodes) * scale
         self.b_hy = np.random.randn(
-            output_size, num_hidden_nodes) * scale
+            self.output_size, self.num_hidden_nodes) * scale
 
     def init_states(
             self,
-            output_size,
-            num_hidden_states,
-            num_hidden_nodes=1) -> None:
+            time_steps,
+            ) -> None:
         """
         Initialises states and assign them to instance variables.
 
@@ -405,10 +399,14 @@ _
         -------------------------------
         None
         """
-        self.num_hidden_nodes = num_hidden_nodes
-        self.hs = np.zeros((num_hidden_states, num_hidden_nodes))
-        self.xs = np.zeros((num_hidden_states, num_hidden_nodes))
-        self.ys = np.zeros((num_hidden_states, output_size))
+        if self.built:
+            prev_h = self.hs[-1]
+        self.hs = np.zeros((time_steps, self.num_hidden_nodes))
+        if self.built:
+            self.hs[-1] = prev_h
+        self.xs = np.zeros((time_steps, self.num_hidden_nodes))
+        self.ys = np.zeros((time_steps, self.output_size))
+        self.built = True
 
     def loss(self, y_true, y_pred, epoch):
         loss = self._loss_function(y_true, y_pred)
