af0943b508ae29165cdfabc5bc19d5720ca7ec62
Mons <monsbjerkan@gmail.com>
2024-03-31 22:29:09 +0200

    Improvements to tests, main_test_sine imploemented new improved plotting

--- main_test_sine.py
+++ main_test_sine.py
@@ -14,144 +14,80 @@ def create_sines(examples=10, seq_length=100):
     X = []
     y = []
     for _ in range(examples):
-        example_x = (np.random.random_sample())*np.sin(np.linspace(0, 8* np.pi, 4*seq_length+1))
+        example_x = np.array([np.sin(np.linspace(0, 8* np.pi, seq_length+1))]).T
         X.append(example_x[0:-1])
         y.append(example_x[1:])
     
-    return np.array([X]), np.array([y])
+    return np.array(X), np.array(y)
 
+#Sine creation
+seq_length = 100
+examples = 1
 
-seq_length = 20
-examples = 50
-epo = 400
+#Prediction
+seed_length = 30
+time_steps_to_predict = seq_length - seed_length
 
-hidden_nodes = 50
-rnn = RNN(
-    hidden_activation='Tanh()',
-    output_activation='Tanh()',
-    loss_function='mse()',
-    optimiser='AdaGrad()',
-    regression=True)
+#RNN init
+epo = 1000
+hidden_nodes = 100
+num_backsteps = seed_length
+learning_rates = [0.003]#[0.001, 0.003, 0.005, 0.01]
+optimisers = ['AdaGrad()', 'SGD()', 'SGD_momentum()']
+
+#Plotting
+offset = 3
 
 X, y = create_sines(examples=examples, seq_length=seq_length)
 
 #Plotting the sine waves that are passed as training data
-for sine in X[0]:
+plt.title("Randomized sines used for training")
+plt.ylabel("Amplitude(y)")
+plt.xlabel("Time(t)")
+
+for sine in X:
     plt.plot(sine)
 plt.show()
 
-X_seed, y_seed = create_sines(examples=1, seq_length=seq_length)
-
-hidden_state = rnn.fit(
-    X, y, epo, learning_rate=0.0001,
-    num_hidden_nodes=hidden_nodes, return_sequences=True)
+X_val = np.array([np.sin(np.linspace(0, 8* np.pi, seq_length+1))]).T
+X_seed = np.array([X_val[:seed_length]])
 
 
-ret = rnn.predict(X_seed)
-plt.plot(ret[0])
-plt.show()
+for learning_rate_curr in learning_rates:
+    print(f'learning rate: {learning_rate_curr}')
 
-epo = 1000
-hidden_nodes = 300
-learning_rates = [0.001, 0.003, 0.005, 0.01]
+    fig_loss, ax_loss = plt.subplots()
+    fig_pred, ax_pred = plt.subplots()
+    
+    ax_pred.set_title(f"Predictions | learning rate: {learning_rate_curr}")
+    ax_pred.set_yticks([])
+    ax_pred.set_xlabel("Time(t)")
+    ax_pred.axvline(x=seed_length-1, color='black', linestyle='--')
 
-#for learning_rate_curr in learning_rates:
-#    fig, ax = plt.subplots()
-#    print(f'learning rate: {learning_rate_curr}')
-#    rnn = RNN(
-#        hidden_activation='Tanh()',
-#        output_activation='Identity()',
-#        loss_function='mse()',
-#        optimiser='AdaGrad()',
-#        regression=True,
-#        threshold=1,
-#        )
-#
-#    whole_sequence_output, hidden_state = rnn.fit(
-#        X, y, epo,
-#        num_hidden_nodes=hidden_nodes, return_sequences=True,
-#        independent_samples=True, learning_rate=learning_rate_curr)
-#
-#    rnn.plot_loss(plt, figax=(fig, ax), show=False)
-#
-#    predict = rnn.predict(X_seed)
-#    plt.plot(predict)
-#
-#    rnn = RNN(
-#        hidden_activation='Tanh()',
-#        output_activation='Identity()',
-#        loss_function='mse()',
-#        optimiser='SGD()',
-#        regression=True,
-#        threshold=1,
-#        )
-#
-#    whole_sequence_output, hidden_state = rnn.fit(
-#        X, y, epo,
-#        num_hidden_nodes=hidden_nodes, return_sequences=True,
-#        independent_samples=True, learning_rate=learning_rate_curr
-#        )
-#
-#    rnn.plot_loss(plt, figax=(fig, ax), show=False)
-#
-#    predict = rnn.predict(X_seed)
-#    plt.plot(predict)
-#
-#    rnn = RNN(
-#        hidden_activation='Tanh()',
-#        output_activation='Identity()',
-#        loss_function='mse()',
-#        optimiser='SGD_momentum()',
-#        regression=True,
-#        threshold=1,
-#        )
-#
-#    whole_sequence_output, hidden_state = rnn.fit(
-#        X, y, epo,
-#        num_hidden_nodes=hidden_nodes, return_sequences=True,
-#        independent_samples=True, learning_rate=learning_rate_curr,
-#        momentum_rate=0.9)
-#
-#    rnn.plot_loss(plt, figax=(fig, ax), show=False)
-#
-#    predict = rnn.predict(X_seed)
-#    plt.plot(predict)
-#
-#    rnn = RNN(
-#        hidden_activation='Tanh()',
-#        output_activation='Identity()',
-#        loss_function='mse()',
-#        optimiser='RMSProp()',
-#        regression=True,
-#        threshold=1,
-#        )
-#
-#    whole_sequence_output, hidden_state = rnn.fit(
-#        X, y, epo,
-#        num_hidden_nodes=hidden_nodes, return_sequences=True,
-#        independent_samples=True, learning_rate=learning_rate_curr,
-#        decay_rate=0.001)
-#
-#    rnn.plot_loss(plt, figax=(fig, ax), show=True)
-#
-#    predict = rnn.predict(X_seed)
-#    plt.plot(predict)
+    for optimiser,i in zip(optimisers, range(len(optimisers))):
+        rnn = RNN(
+            hidden_activation='Tanh()',
+            output_activation='Identity()',
+            loss_function='mse()',
+            optimiser=optimiser,
+            clip_threshold=1,
+            )
 
+        hidden_state = rnn.fit(
+            X, y, epo, learning_rate=learning_rate_curr,
+            num_hidden_nodes=hidden_nodes, return_sequences=True,
+            num_backsteps=num_backsteps)
 
-# X, y = create_sines(examples=examples, seq_length=50)
-# rnn.fit(X, y, epo, num_hidden_states=50, num_hidden_nodes=hidden_nodes)
-# y_pred_val = rnn.predict(X_val)
-# plt.plot(X_val[0], label='X_val', linestyle='solid', color='red')
-# plt.plot(y_pred_val, label='y_pred_val', linestyle='dotted', color='blue')
-# plt.plot(y_val[0], label='y_val')
-# plt.legend()
-# plt.show()
+        rnn.plot_loss(plt, figax=(fig_loss, ax_loss), show=False)
 
-# stats = rnn.get_stats()
-# plt.plot(stats['loss'], label='loss')
-# plt.legend()
-# plt.show()
-# plt.plot(ret, label='output sine')
-# plt.plot(y, label='true sine')
-# plt.legend()
-# plt.show()
+        predict = rnn.predict(X_seed, time_steps_to_generate=time_steps_to_predict)
+
+        plot_line = np.concatenate((X_seed[0],predict))
+        ax_pred.plot(plot_line - (i+1)*offset, label=str(rnn._optimiser.__class__.__name__))
+        ax_pred.legend()
+    
+    ax_pred.plot(X_val, label = "X val")
+    ax_pred.legend()
+
+    plt.show()
+
