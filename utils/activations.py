2a55e33326a4b69149ecdf52e38d85df90273598
Mons <monsbjerkan@gmail.com>
2024-05-14 10:43:21 +0200

    Removed jax from activations.py

--- utils/activations.py
+++ utils/activations.py
@@ -1,8 +1,6 @@
 import numpy as np
 from scipy.special import expit  # used for sigmoid
 from collections.abc import Callable
-import jax.numpy as jnp
-from jax import jit
 # https://dustinstansbury.github.io/theclevermachine/derivation-common-neural-network-activation-functions
 
 """
@@ -37,13 +35,13 @@ class Relu(Activation):
         """Returns a np.ndarray with same dimensions as z"""
         # ret = [n * (n > 0) for n in z]
         # return np.array(ret, dtype=float)
-        # self.z = jnp.maximum(0, z)
-        return jnp.maximum(0, z)
+        # self.z = np.maximum(0, z)
+        return np.maximum(0, z)
         # return self.z
 
     def grad(self, a):
         """Decided on using f'(0)=1. Could do f'(0)=0 instead"""
-        return jnp.array([1 if i >= 0 else 0 for i in a])
+        return np.array([1 if i >= 0 else 0 for i in a])
 
 
 class Tanh(Activation):
@@ -53,7 +51,7 @@ class Tanh(Activation):
 
     def eval(self, z):
         """Returns a np.ndarray with same dimensions as z"""
-        return jnp.tanh(z)
+        return np.tanh(z)
 
     def grad(self, a):
         """
@@ -86,7 +84,7 @@ class Softmax(Activation):
         super().__init__()
 
     def eval(self, z):
-        softmax = jnp.exp(z)/jnp.sum(jnp.exp(z))
+        softmax = np.exp(z)/np.sum(np.exp(z))
         return softmax
 
     def grad(self, a):
@@ -95,7 +93,7 @@ class Softmax(Activation):
         derivative must be calculated differently
         """
         s = a.reshape(-1, 1)
-        return jnp.diagflat(s) - jnp.dot(s, s.T)
+        return np.diagflat(s) - np.dot(s, s.T)
 
 # soft = Softmax()
 # data = [1/np.e,1/np.e,1/np.e]
