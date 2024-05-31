b292907501f7cc38542172a9fdb429854e1474e2
Trym Auren <trym.auren@gmail.com>
2024-05-31 23:29:15 +0200

    Delete another folder

--- testing/run_tests.py
+++
@@ -1,9 +0,0 @@
-import git
-import sys
-from testing import tests
-path_to_root = git.Repo('.', search_parent_directories=True).working_dir
-sys.path.append(path_to_root)
-
-
-tests.test_activations()
-tests.test_read_save()
